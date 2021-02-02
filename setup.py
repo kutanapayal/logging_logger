c1ecd70a92ecc216b8b7f0d86f4694337c76567a
payal kutana <46380440+payalkutana@users.noreply.github.com>
2021-02-02 21:26:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'logging-logger',         
+  packages = ['logging-logger'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'loggiing in python made easy - receive the log file you like in just one line of code!',   
+  author = 'payal kutana',                   
+  author_email = 'kutanapayal@gmail.com',    
+  url = 'https://github.com/payalkutana/logging-logger',  
+  download_url = 'https://github.com/payalkutana/logging-logger/archive/logging-logger_01.tar.gz',   
+  keywords = ['logger','logging','logs','easy'],   
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
