6ff3f01ec82393ac87f311a6d6dbe2167bd2a2fc
payal kutana <46380440+payalkutana@users.noreply.github.com>
2021-02-03 12:36:24 +0530

    Delete demo.py directory

--- demo.py/test.py
+++
@@ -1,8 +0,0 @@
-from logging_logger import loggerClass
-import logging
-
-loggerClass.WritetoScreen(loggerClass,logging.INFO,"testing...",
-            '%(levelname)s:%(message)s')
-
-loggerClass.Writetofile(loggerClass,'sample.log',logging.WARNING,"testing...",
-            '%(levelname)s:%(message)s')
